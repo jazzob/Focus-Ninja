be094449dff59848bc3e2f5a996cf83c6ffca26f
Jazzo201060 <114203544+jazzob@users.noreply.github.com>
2023-07-13 22:14:23 +1200

    base enemy code

---
+++ focus-ninja-v2/src/actors/Enemy.gd
@@ -0,0 +1,16 @@
+extends "res://src/actors/actor_script.gd"
+
+#sets enemy direction for start
+func _ready() -> void:
+	#ensures the enemy does not start moving until in view
+	set_physics_process(false)
+	velocity.x = -speed
+
+#creates enemy movement
+func _physics_process(delta: float) -> void:
+	velocity.y = gravity * delta
+	if is_on_wall():
+		velocity.x *= -1.0
+		
+	velocity.y = move_and_slide(velocity, UP_DIRECTION).y
+	
