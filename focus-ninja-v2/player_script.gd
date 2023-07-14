b758fc0eb565c55a5fa0f78f6aa2b10df04a38bf
Jazzo201060 <114203544+jazzob@users.noreply.github.com>
2023-07-14 21:49:08 +1200

    finished version 2

--- focus-ninja-v2/player_script.gd
+++ focus-ninja-v2/player_script.gd
@@ -1,4 +1,6 @@
 extends Actor
+class_name Player
+
 
 #creates movement
 func _physics_process(delta: float) -> void:
@@ -7,8 +9,6 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("move_right")
 		- Input.get_action_strength("move_left")
 	)
-	
-	
 	
 	#calculates movement
 	velocity.x = horizontal_direction * speed
@@ -19,3 +19,24 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -jump_strength
 		
 	velocity = move_and_slide(velocity, UP_DIRECTION)
+	
+	#code for stomp ability
+	for i in get_slide_count():
+		var collision := get_slide_collision(i)
+		var collider := collision.collider
+		var is_stomping:= (
+			collider is Enemy and is_on_floor() and collision.normal.is_equal_approx(Vector2.UP)
+		)
+		
+		if is_stomping:
+			#bounce from stomp 
+			velocity.y = -stomp_impulse
+			#calling kill func
+			(collider as Enemy).kill()
+			
+
+
+#detects if the enemy enters the enemy detector area and kills player
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body is Enemy:
+		queue_free()
--- focus-ninja-v2/src/actors/Enemy.gd
+++ focus-ninja-v2/src/actors/Enemy.gd
@@ -1,10 +1,12 @@
-extends "res://src/actors/actor_script.gd"
+extends Actor
+class_name Enemy
 
 #sets enemy direction for start
 func _ready() -> void:
 	#ensures the enemy does not start moving until in view
 	set_physics_process(false)
 	velocity.x = -speed
+
 
 #creates enemy movement
 func _physics_process(delta: float) -> void:
@@ -13,4 +15,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x *= -1.0
 		
 	velocity.y = move_and_slide(velocity, UP_DIRECTION).y
-	
+
+#when called kills the enemy
+func kill() -> void:
+	queue_free()
+
+
