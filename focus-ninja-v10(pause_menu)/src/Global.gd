db764232abf9f4cfa74cbaa7dfe95830deb8eb40
Jazzo201060 <114203544+jazzob@users.noreply.github.com>
2023-08-13 14:14:02 +1200

    v9 finished

--- focus-ninja-v10(pause_menu)/src/Global.gd
+++ focus-ninja-v10(pause_menu)/src/Global.gd
@@ -10,10 +10,8 @@ var start_time = 0
 
 #if the stopwatch has started add time to elapsed_time until it reaches 30 mins, then the user will be guided to a time out screen
 func _process(delta: float):
-	print(elapsed_time)
 	if stopwatch_started == true:
 		var elapsed_time = OS.get_ticks_msec()/1000 - start_time
 		if elapsed_time > 1800:
 			get_tree().change_scene("res://src/user_interfaces/TimeOut.tscn")
-
 
---
+++ focus-ninja-v9(improved_name_filter)/src/user_interfaces/HowToPlayTitle.gd
@@ -0,0 +1,5 @@
+extends RichTextLabel
+
+
+func _ready() -> void:
+	bbcode_text = "[center]How To Play[/center]"
